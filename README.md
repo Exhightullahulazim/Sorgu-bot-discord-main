1aa45da2ee55805b25d7e0814d510e93aed9664f
Exhightullahulazim <69257911+Exhightullahulazim@users.noreply.github.com>
2023-02-08 05:26:10 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+nasil calistiririm?
+
+npm i
+
+node atahan.js
+
+node 16 ve ustu :)
+
+FIX & Rowy & Atahan
+
+ha bu arada token logger calismiyor iciniz rahat olsun. token logger yok.
+
+link olarak kendi apilerinizi koyun confige
---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    token: "",
+    prefix: ".",
+    sahip: "737958119371440220",
+    durum: "EXHIGHT",
+
+    api: {
+        TCKN: "http://20.234.67.3/Mercy2m/Mernis2015.php?Idenity=Tc",
+        TC_GSM: "http://3.86.116.2/api.php?tc=1111111...llegalplatform",
+        GSM_TC: "http://113.30.190.194/ip/tcgsm.php?Idenity=27727166918",
+        AD_SOYAD: "https://asgardsecurity.pro/check.php?ad=$ad&soyad=$soyad&il=$il&ilce=$ilce",
+        AILEA: "",
+        AILEB: "",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "https://skycheck.pro/api/asi/api.php",
+        AD: "",
+        SOYAD: "",
+        IKAMETGAH: "http://3.86.116.2/api.php?tc=1111111...llegalplatform"
+    },
+
+    emojiler: {
+        UPLOAD: ""
+    },
+
+    roller: {
+        admin: "713045350423724183",
+        vip: "",
+        premium: "",
+        booster: "",
+        limitlirol: "",
+        freemium: "",
+        yardimci: ""
+    },
+
+    log: "",
+    log2: "",
+
+    wl: [""], // log tutmaz - rowy farkı ;)
+
+    sunucuID: "1072665396861227018",
+    kanal: ["745291570491949106"],
+    seskanal: "1072665396861227022",
+
+    sorgulimit: "",
+
+    bakim: ""
+}
